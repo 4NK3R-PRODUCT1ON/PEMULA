e4c565eb4fe65b839120c8ee72d630a12d5700cf
Anker production <67215583+4NK3R-PRODUCT1ON@users.noreply.github.com>
2020-08-01 08:19:52 +0700

    Add files via upload

---
+++ password.py
@@ -0,0 +1,70 @@
+import os, sys
+
+print ("\033[1;32mSilahkan Masukkan Username & Password Anda")
+
+print ("\033[1;32mau tau silahkan Hubungi  +1 (234) 985-0607 ")
+
+
+print ("\033[1;32mScript Ini Sekarang Tidak Gratis Susah Buatnya ")
+
+username = 'TOOLS'      
+
+password = 'IDULADHA'
+
+
+
+def restart():
+
+	ngulang = sys.executable
+
+	os.execl(ngulang, ngulang, *sys.argv)
+
+
+
+def main():
+
+	uname = raw_input("username : ")
+
+	if uname == username:
+
+		pwd = raw_input("password : ")
+
+
+
+		if pwd == password:
+
+			print "\033[1;32mAlhmdllh sudah masuk juga..", 
+
+			sys.exit()
+
+
+
+		else:
+
+			print "\033[1;32mMaaf Masukkan password Anda salah... [?]\033[00m"
+
+			print "Silahkan segera log-in kembali...!!\n"
+
+			restart()
+
+
+
+	else:
+
+		print "\033[1;32mMaaf Masukkan Username Anda salah... [?]\033[00m"
+
+		print "Silahkan segera log-in kembali...!!\n"
+
+		restart()
+
+
+
+try:
+
+	main()
+
+except KeyboardInterrupt:
+
+	os.system('clear')
+
+	restart()
---
+++ pemula.sh
@@ -0,0 +1,13 @@
+clear
+echo
+echo
+echo
+clear
+echo
+cyan='\033[36;1m'
+ungu='\033[35;1m'
+figlet PENGINSTALAN | lolcat
+figlet SELESAI | lolcat
+figlet SUBSCRIBE | lolcat
+figlet ANKER | lolcat
+figlet PRODUCTION | lolcat
